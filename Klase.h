5fb896b1bf763e9532aa791f2eb367d077ea0ccd
Mihailo Plavsic <49532083+MihailoPlavsic34@users.noreply.github.com>
2019-10-29 15:56:08 +0100

    Update Klase.h

--- Klase.h
+++ Klase.h
@@ -4,19 +4,18 @@ using namespace std;
 
 class Pilot {
 public:
-	Pilot() { ime = ""; sati = 0; bool letilit = false; };  //podrazumevani
-	Pilot(string pime, int psati, bool pletili = false) {  //konvertujuci, pletili podrazumevano false
+	Pilot() { ime = ""; sati = 0; bool letilit = false; };  
+	Pilot(string pime, int psati, bool pletili = false) {  
 		ime = pime;
 		sati = psati;
 		letili = pletili;
 	};
-	~Pilot() {};  //destruktor, nek sttoji?
 	void printpilot() const;
 	void povecajsati(int s);
-	int satii();  //moze se dohvatiti
-	bool letilii();  //moze da dohvati
+	int satii();  
+	bool letilii();  
 	void promeniletili(bool a);
-	string imee();  //moze se dohvatiti
+	string imee();  
 private:
 	string ime;
 	int sati;
@@ -25,49 +24,51 @@ private:
 
 class Avion {
 public:
-	Avion() { naziv = ""; max = 0; kapetan, kopilot = nullptr; };  //podrazumevani
-	Avion(string anaziv, int amax, Pilot*kapetan=nullptr, Pilot*kopilot=nullptr) {  //konvertujuci, da li ako ovde ne definisem nista za pilot i kopilot je nullptr ili ne? 
-																					// #1edit: Da jer si ih inicijalizovala dole kao null. Da nisi to uradila morala bi da u def napises =nullptr kao sto je dodato!
-		naziv = anaziv;																//ovo ima svoje mane jer sad ili moras da izostavis sve pok u pozivu konstruktora, ili samo kopilota, prica iz knjige za poz f-ja sa predef vrednostima!
-		max = amax;																	// mozda je bolje da ostane funkcija kakva jeste, ida samo dodas dole kopilot = nullptr; kapetan = nullptr;
-		kapetan = nullptr;															//tada svi konstruktori koji ne inicijalizuju ta polja ostavljaju vrednosti nullptr zaci NOJS
-		kopilot = nullptr;															//ja sam ostavio sav tri nacina ti ostavi onaj koji ti se vise svidja! Malo konfuzno nemam mesta lepo uzivo cemo :)
+	Avion() { naziv = ""; max = 0; kapetan, kopilot = nullptr; }; 
+	Avion(string anaziv, int amax, Pilot* kapetan = nullptr, Pilot* kopilot = nullptr) {  
+																					
+		naziv = anaziv;																
+		max = amax;																	
+		kapetan = nullptr;															
+		kopilot = nullptr;															
 	};
-	~Avion() { delete kapetan, delete kopilot; };  //destruktor?
+	~Avion() { delete kapetan, delete kopilot; };  
 	void printavion() const;
 	void kapetann();
 	void kopiloot();
-	string nazivv(); // **Pataric glas** NaziIIVVV, MihaIIIlOOOO
+	string nazivv(); 
 	void kapetanaviona(Pilot* kapetann);
 	void kopilotaviona(Pilot* kopilott);
 	int maxx();
 private:
 	string naziv;
-	int max;  //maksimalan broj putnika koje moze da primi
-	Pilot* kapetan=nullptr;
-	Pilot* kopilot=nullptr;
+	int max;  
+	Pilot* kapetan = nullptr;
+	Pilot* kopilot = nullptr;
 };
 
-class Flota {
+class Flota { ///poptaviii
 public:
-	Flota() { naziv = ""; int broj = 0;  avioni = nullptr; };  //podrazumevani
-	Flota(string fnaziv) {  //konvertujuci, avioni se naknadno dodaju
-		naziv = fnaziv; //morala bi da dodas kao dole pod //## sto sam stavio jer ovim pozivom ti ostaje pok avioni neinicijalizovan!
-						//mogla si i u ovom konstruktoru bez problema to al ja voim dole jer se onda odnosi na sve konstruktore, pa ako nekad zab ne puca program i to!
+	Flota() { naziv = ""; int broj = 0;  avioni = nullptr; };  
+	Flota(string fnaziv) { 
+		naziv = fnaziv;
+						
 	};
-	Flota(const Flota& Flota) {  //kopirajuci
-		for (int i = 0; i < broj; i++) { avioni[i] = Flota.avioni[i]; }; //samo me zanima gde si inicijalizovala onaj avioni niz, jer ovako presipas u suplju memoriju!
-		broj = Flota.broj;												//i da bukvalno imam istu gresku... bravo Mihailo!				
+
+	Flota(const Flota& Flota) {  
+		avioni = new Avion * [broj];
+		for (int i = 0; i < broj; i++) { avioni[i] = Flota.avioni[i]; }; 
+		broj = Flota.broj;														
 		naziv = Flota.naziv;
 	};
-	~Flota() { delete[] avioni; avioni = nullptr; broj = 0; }; //destruktor obavezno!!!
+	~Flota() { delete[] avioni; avioni = nullptr; broj = 0; }; 
 	void printflota();
 	int ukupnoaviona();
 	int maxputnika();
 	int findbiggest();
-	void addplane(Avion* a);
+	void addplanes(Avion** avionii);
 private:
 	string naziv;
 	int broj;
-	Avion** avioni =nullptr; //##
+	Avion** avioni = nullptr; 
 };
